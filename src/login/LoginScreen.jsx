c744c9e3027304abc1657262b5197506eddb094c
avargasm1 <avargasm@unsa.edu.pe>
2022-02-08 13:46:05 -0500

    Login Screen Finalizado V2.00

--- src/login/LoginScreen.jsx
+++ src/login/LoginScreen.jsx
@@ -13,63 +13,68 @@ const LoginScreen = () => {
 
     return (
 
-        <ScrollView style={{ flex: 1, backgroundColor: '#01286B' }}>
-           
-                <ImageBackground source={image} style={styles.image}>
+        <ScrollView style={{ flex: 1, backgroundColor: '#02286B' }}>
 
-                    <View style={styles.portadaView}>
-                        <Text style={styles.portadaViewText}>
-                            RUT-OP
-                        </Text>
-                        <Text style={styles.portadaViewTextSmall}>
-                            Registro de Rutinas Operacionales
-                        </Text>
-                        <Image source={logo_blanco} style={styles.logo_blanco} />
-                    </View>
+            <ImageBackground source={image} style={styles.image}>
 
-                </ImageBackground>
-
-                {/* Aqui sigue el cuerpo de bienvenida */}
-                <View style={styles.bodyContainer}>
-
-                    <View style={{ padding: 40 }}>
-                        <Text style={{ color: "white", fontSize: 34, fontWeight: '500', fontFamily: 'Roboto' }}>
-                            Bienvenido
-                        </Text>
-
-                        <Text style={{ color: 'white', fontWeight: '300', fontFamily: 'Roboto', fontSize: 14 }}>
-                            Iniciar Sesión para Continuar
-                        </Text>
-                        <>
-                            <View style={{ marginTop: 50 }}>
-
-                                <FormControl>
-
-                                    <Stack space={5}>
-
-                                        <Stack backgroundColor={"#023285"} style={styles.cajasTexto}>
-
-                                            <FormControl.Label _text={{ color: '#669EFF', fontWeight: 400, fontSize: 14 }}>USUARIO</FormControl.Label>
-
-
-
-                                            <Input fontSize={16} color={'white'} variant="underlined" InputLeftElement={<Icon as={<FontAwesomeIcon name="user" style={styles.iconUser} />} size={2} />} p={2} placeholder="usuarioantapaccay1" />
-                                        </Stack>
-                                        <Stack backgroundColor={"#023285"} style={styles.cajasTexto}>
-                                            <FormControl.Label _text={{ color: '#669EFF', fontWeight: 400, fontSize: 14 }}>CONTRASEÑA</FormControl.Label>
-                                            <Input fontSize={16} color={'white'} variant="underlined" InputLeftElement={<Icon as={<FontAwesomeIcon name="lock" style={styles.iconUser} />} size={2} />} p={2} placeholder="*******" />
-                                        </Stack>
-                                        <Button onPress={() => console.log("hello world")} backgroundColor={'white'} _text={{ color: '#01286B' }}>INGRESAR</Button>
-                                    </Stack>
-
-                                </FormControl>
-
-
-                            </View>
-                        </>
-                    </View>
+                <View style={styles.portadaView}>
+                    <Text style={styles.portadaViewText}>
+                        RUT-OP
+                    </Text>
+                    <Text style={styles.portadaViewTextSmall}>
+                        Registro de Rutinas Operacionales
+                    </Text>
+                    <Image source={logo_blanco} style={styles.logo_blanco} />
                 </View>
-            
+
+            </ImageBackground>
+
+            {/* Aqui sigue el cuerpo de bienvenida */}
+            <View style={styles.bodyContainer}>
+
+                <View style={{ padding: 40 }}>
+                    <Text style={{ color: "white", fontSize: 34, fontWeight: '500', fontFamily: 'Roboto', ineHeight: 28.13 }}>
+                        Bienvenido
+                    </Text>
+
+                    <Text style={{ color: 'white', fontWeight: '300', fontFamily: 'Roboto', fontSize: 14, lineHeight: 16.41 }}>
+                        Iniciar Sesión para Continuar
+                    </Text>
+                    <>
+                        <View style={{ marginTop: 50 }}>
+
+                            <FormControl>
+
+                                <Stack space={5}>
+
+                                    <Stack backgroundColor={"#023285"} style={styles.cajasTexto}>
+
+                                        <FormControl.Label _text={{ color: '#669EFF', fontWeight: 400, fontSize: 14 }}>USUARIO</FormControl.Label>
+
+
+
+                                        <Input fontSize={16} color={'white'} variant="underlined" InputLeftElement={<Icon as={<FontAwesomeIcon name="user" style={styles.iconUser} />} size={2} />} p={2} placeholder="usuarioantapaccay1" />
+                                    </Stack>
+                                    <Stack backgroundColor={"#023285"} style={styles.cajasTexto}>
+                                        <FormControl.Label _text={{ color: '#669EFF', fontWeight: 400, fontSize: 14 }}>CONTRASEÑA</FormControl.Label>
+                                        <Input fontSize={16} color={'white'} variant="underlined" InputLeftElement={<Icon as={<FontAwesomeIcon name="lock" style={styles.iconUser} />} size={2} />} p={2} placeholder="*******" />
+                                    </Stack>
+                                    <Button onPress={() => console.log("hello world")} backgroundColor={'white'} _text={{ color: '#01286B' }}>INGRESAR</Button>
+                                </Stack>
+
+                            </FormControl>
+
+
+                        </View>
+                    </>
+                </View>
+                <View  style={{ flex:1, justifyContent: 'center', alignItems: 'flex-end', marginRight:30, marginBottom:20 }}>
+                    <Text style={{ color: "white", textAlign:'center', fontSize: 11, fontWeight: '300', fontFamily: 'Roboto', ineHeight: 12.89 }}>
+                        Versión 1.0
+                    </Text>
+                </View>
+            </View>
+
         </ScrollView>
 
 
@@ -102,9 +107,11 @@ const styles = StyleSheet.create({
     },
     portadaView: {
         flex: 1,
+
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'rgba(1, 40, 107, 0.5)',
+
 
     },
     logo_blanco: {

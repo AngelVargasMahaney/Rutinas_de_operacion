476282698465ea959d9f0dfb24267b26ec5d9e20
unknown <stevencordovap@gmail.com>
2022-02-09 10:39:02 -0500

    TemplateScreen V1.03

--- src/Template/TemplateScreen.jsx
+++ src/Template/TemplateScreen.jsx
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-
+import { Menu, Provider } from 'react-native-paper';
 const image = require('../../assets/backgrounds/Pantalla_login.png')
 const logo_color = require('../../assets/logos/Logo_color.png')
 
 const TemplateScreen = () => {
+
+    const [estadoInicial, setEstadoInicial] = useState(true)
+
+    _openMenu = () => (
+        setEstadoInicial(true)
+    );
+
+    _closeMenu = () => (
+        setEstadoInicial(false)
+    );
+
     return (
         <View style={styles.container}>
             <Text style={styles.rutOp}>RUT-OP</Text>
@@ -16,7 +27,17 @@ const TemplateScreen = () => {
                     resizeMode="contain"
                     style={styles.image}
                 ></Image>
-                <Icon name="dots-vertical" style={styles.icon}></Icon>
+                <Provider>
+                    <Menu
+                        visible={estadoInicial}
+                        onDismiss={_closeMenu}
+                        anchor={
+                            <Icon name="dots-vertical" style={styles.icon} onPress={_openMenu}></Icon>
+                        }
+                    >
+                        <Menu.Item onPress={() => { }} title="Cerrar Sesion" />
+                    </Menu>
+                </Provider>
             </View>
         </View>
     );
@@ -29,34 +50,34 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "rgba(255,255,255,1)",
         flexDirection: "row"
-      },
-      rutOp: {
+    },
+    rutOp: {
         fontFamily: "roboto-700",
         color: "rgba(1,40,107,1)",
         fontSize: 30,
         marginLeft: 25,
         marginTop: 81
-      },
-      rutOpFiller: {
+    },
+    rutOpFiller: {
         flex: 1,
         flexDirection: "row"
-      },
-      image: {
+    },
+    image: {
         width: 84,
         height: 43,
         marginRight: 5,
         marginTop: 29
-      },
-      icon: {
+    },
+    icon: {
         color: "rgba(1,40,107,1)",
         fontSize: 27
-      },
-      imageRow: {
+    },
+    imageRow: {
         height: 72,
         flexDirection: "row",
         marginRight: 16,
         marginTop: 49
-      }
-    });
-    
-    export default TemplateScreen;
+    }
+});
+
+export default TemplateScreen;

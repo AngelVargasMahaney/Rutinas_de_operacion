33d789ade765d804df50a23b7791259896f697a6
avargasm1 <avargasm@unsa.edu.pe>
2022-02-09 11:55:40 -0500

    Añadido el templateV2 para las vistas

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import LoginScreen from './src/login/LoginScreen';
 import ContenedorScreens from './src/module 1/ContenedorScreens';
+import TemplateScreen from './src/Template/TemplateScreen';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import TemplateVersion2 from './src/Template/TemplateVersion2';
 
 export default function App() {
   return (
     // <ContenedorScreens />
-    <NativeBaseProvider>
-        <ContenedorScreens />
-    </NativeBaseProvider>
+    // <NativeBaseProvider>
+    //     <TemplateScreen />
+    // </NativeBaseProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <TemplateVersion2 />
+      </ApplicationProvider>
+    </>
   );
 }
 
